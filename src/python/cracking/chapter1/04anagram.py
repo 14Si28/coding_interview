6c381151f154ef200e7edbfa9dd039a1fa76e464
Michael Lossos <mlossos@pobox.com>
2012-01-23 21:21:45 -0500

    Added anagrams and replace spaces.

---
+++ src/python/cracking/chapter1/04anagram.py
@@ -0,0 +1,68 @@
+"""
+Write a method to decide if two strings are anagrams or not.
+"""
+
+def is_anagram_cheat(a, b):
+    """
+    Convert strings to lists, sort them, convert back, compare. The conversions make this ugly.
+    """
+    if a == b:
+        return False
+    if len(a) != len(b):
+        return False
+    a_sorted = sorted([c for c in a])
+    b_sorted = sorted([c for c in b])
+    return ''.join(a_sorted) == ''.join(b_sorted)
+
+def is_anagram_naive(a, b):
+    """
+    Naive anagram check. O(n), wasted storage for counts, worst case 3x iterations.
+    """
+    if a == b:
+        return False
+    if len(a) != len(b):
+        return False
+
+    a_count = {}
+    for c in a:
+        a_count.setdefault(c, 0)
+        a_count[c] += 1
+    b_count = {}
+    for c in b:
+        b_count.setdefault(c, 0)
+        b_count[c] += 1
+
+    for k,v in a_count.iteritems():
+        if not k in b_count or b_count[k] != v:
+            return False
+    return True
+
+
+#################################
+# Tests
+#
+
+def _check_anagram_func(func, a, b, expected):
+    actual = func(a, b)
+    if actual != expected:
+        raise AssertionError(
+            'FAIL: Expected: {}   Actual: {}   Input a: {}  Input b: {}'.format(expected, actual, a, b))
+
+def _test_anagram_all(func):
+    _check_anagram_func(func, '', '', False)
+    _check_anagram_func(func, 'a', 'b', False)
+    _check_anagram_func(func, 'ab', 'ba', True)
+    _check_anagram_func(func, 'abb', 'bab', True)
+    _check_anagram_func(func, 'abba', 'baba', True)
+    _check_anagram_func(func, 'catalogue', 'coagulate', True)
+    _check_anagram_func(func, 'basiparachromatin', 'marsipobranchiata', True)
+    _check_anagram_func(func, 'undefinability', 'unidentifiably', True)
+
+
+if __name__ == '__main__':
+    _test_anagram_all(is_anagram_naive)
+    _test_anagram_all(is_anagram_cheat)
+    print 'SUCCESS'
+
+
+
---
+++ src/python/cracking/chapter1/05replacespaces.py
@@ -0,0 +1,57 @@
+"""
+Write a method to replace all spaces in a string with %20.
+"""
+
+def replace_spaces_cheat(s):
+    return s.replace(' ', '%20')
+
+# Don't ever do this in python.
+def replace_spaces_array(s):
+    # If you MUST use primitive arrays and indexes, and cannot use array's insert...
+    orig_arr  = [c for c in s]
+    spaces_count = len([c for c in s if c == ' '])
+    new_space = '%20'
+    import array # Import should be at top. Not following PEP-8 here so we colocate the ugly import of array.
+    new_len = len(s) + spaces_count*(len(new_space)-1) # len(s) counts the existing spaces once, new spaces size = (3 - 1)
+    new_arr  = array.array('c', (' ',)*new_len) # 'c: char array, ('',) tuple for init each elem, * len: make it the same size
+
+    new_index = 0
+    for c in orig_arr:
+        if c == ' ':
+            for space_char in new_space:
+                new_arr[new_index] = space_char
+                new_index += 1
+        else:
+            new_arr[new_index] = c
+            new_index += 1
+
+    return ''.join(new_arr)
+
+#################################
+# Tests
+#
+
+def _check_spaces_func(func, s, expected):
+    actual = func(s)
+    if actual != expected:
+        raise AssertionError(
+            'FAIL: Expected: {}   Actual: {}   Input a: {}  Input b: {}'.format(expected, actual, s))
+
+def _test_spaces_all(func):
+    _check_spaces_func(func, 'hello world', 'hello%20world')
+    _check_spaces_func(func, 'this has spaces', 'this%20has%20spaces')
+    _check_spaces_func(func, ' space at beginning', '%20space%20at%20beginning')
+    _check_spaces_func(func, 'space at end ', 'space%20at%20end%20')
+    _check_spaces_func(func, '   ', '%20%20%20')
+    _check_spaces_func(func, ' ', '%20')
+    _check_spaces_func(func, '', '')
+    _check_spaces_func(func, 'abc', 'abc')
+
+if __name__ == '__main__':
+    _test_spaces_all(replace_spaces_cheat)
+    _test_spaces_all(replace_spaces_array)
+    print 'SUCCESS'
+
+
+
+
