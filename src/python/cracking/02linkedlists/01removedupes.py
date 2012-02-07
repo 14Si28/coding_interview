e84deca3427a0a00e002cf18d611c461a318481c
Michael Lossos <mlossos@pobox.com>
2012-02-06 22:39:09 -0500

    Another linked list remove duplicates impl.

--- src/python/cracking/02linkedlists/01removedupes.py
+++ src/python/cracking/02linkedlists/01removedupes.py
@@ -19,6 +19,24 @@ def remove_duplicates(nodes):
             seen.add(x)
     return result
 
+# Illustrates an algorithm. Don't ever do this in python.
+def remove_duplicates_no_temp(nodes):
+    """
+    Avoid using any temporary storage (no sets).
+    Repeatedly scan the entire list for duplicates of the current node.
+    Remove duplicates from the original list in place, removing the first (not last) of each duplicate.
+
+    nodes: a list of objects. Their __cmp__ or __eq__ (if defined) will be used to determine equality.
+    """
+    index_offset = 0
+    original_len = len(nodes)
+    for index in xrange(original_len):
+        for scan_index in xrange(index+1, original_len):
+            if nodes[index-index_offset] == nodes[scan_index-index_offset]:
+                nodes.pop(index-index_offset)
+                index_offset += 1
+                break
+    return nodes
 
 ## Illustration. Don't do this in python.
 #def ExampleNode(object):
@@ -37,17 +55,6 @@ def remove_duplicates(nodes):
 #
 #    def append(node):
 #        pass
-#
-#
-## Illustrates an algorithm. Don't ever do this in python.
-#def remove_duplicates_no_temp(nodes):
-#    """
-#    Avoid using any temporary storage (no sets).
-#    Repeatedly scan the entire list for duplicates of the current node.
-#    Remove duplicates from the original list in place.
-#    """
-#    pass
-
 
 #################################
 # Tests
@@ -67,9 +74,11 @@ def _test_all(func):
     _check_func(func, [0,0,1], [0,1])
     _check_func(func, [0,1,1], [0,1])
     _check_func(func, [0,1,1,2,3,3,4,5,5,5,6,7,8,8,8], [0,1,2,3,4,5,6,7,8])
+    #_check_func(func, [1,0,1,1], [1,0]) # Expected ordering depends on func removing first or remaining duplicates.
 
 if __name__ == '__main__':
     _test_all(remove_duplicates)
+    _test_all(remove_duplicates_no_temp)
     print 'SUCCESS'
 
 
