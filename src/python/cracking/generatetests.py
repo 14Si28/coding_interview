30a5f8b4124e5aa5cea221008c2ff027e8e923f9
Michael Lossos <mlossos@pobox.com>
2012-02-10 23:55:38 -0500

    Refactored generatetests.

--- src/python/cracking/generatetests.py
+++ src/python/cracking/generatetests.py
@@ -1,5 +1,5 @@
 """
-Generate unittest cases for all the cracking solutions by walking dirs and python files.
+Generate unittest cases for all the Cracking problem solutions by walking dirs and python files.
 """
 import re
 import os
@@ -11,40 +11,53 @@ def _append_blank_lines(output_lines, n):
     for x in xrange(n):
         output_lines.append('')
 
-def main():
-    source_dir = os.path.abspath(path.dirname(__file__))
-    output_lines = []
-    solution_re = re.compile(r'^s.*\.py$')
+def _get_test_map(source_dir):
+    solution_re = re.compile(r'^s.*\.py$') # solution files start with an 's'
     test_map = {}
     for dirpath, dirnames, filenames in os.walk(source_dir):
         package = os.path.basename(dirpath)
         test_files = sorted([os.path.splitext(x)[0] for x in filenames if solution_re.match(x)])
         if test_files:
             test_map.setdefault(package, []).extend(test_files)
+    return test_map
 
-
-    output_lines.append('import unittest')
-    output_lines.append('# Ugly relative imports. These make it easy to run nosetests from the top of the repo, though. Normally, do not use relative imports.')
-
-    test_pairs = sorted(test_map.iteritems())
+def _get_imports(test_pairs):
+    output = [
+        'import unittest',
+        '# Ugly relative imports. These make it easy to run nosetests from the top of the repo, though. Normally, do not use relative imports.'
+    ]
     for package, test_files in test_pairs:
         for file in test_files:
-            output_lines.append('from cracking.{} import {}'.format(package, file))
+            output.append('from cracking.{} import {}'.format(package, file))
+    return output
 
-    _append_blank_lines(output_lines, 3)
-
+def _get_test_classes(test_pairs):
+    output = []
     for package, test_files in test_pairs:
-        output_lines.append('class Test{}(unittest.TestCase):'.format(package))
+        output.append('class Test{}(unittest.TestCase):'.format(package))
         for file in test_files:
-            output_lines.append('    def test_{}(self):'.format(file))
-            output_lines.append('        {}._test_all()'.format(file))
-            _append_blank_lines(output_lines, 1)
+            output.append('    def test_{}(self):'.format(file))
+            output.append('        {}._test_all()'.format(file))
+            _append_blank_lines(output, 1)
+    return output
 
+def _write_output(source_dir, output_lines):
     output_lines = ['{}\n'.format(x) for x in output_lines]
     output_path = '{}/testall.py'.format(source_dir)
     with open(output_path, 'w') as output_file:
         output_file.writelines(output_lines)
     print 'Wrote test cases to: {}'.format(output_path)
+
+def main():
+    source_dir = os.path.abspath(path.dirname(__file__))
+    output_lines = []
+    test_pairs = sorted(_get_test_map(source_dir).iteritems())
+
+    output_lines.extend(_get_imports(test_pairs))
+    _append_blank_lines(output_lines, 3)
+    output_lines.extend(_get_test_classes(test_pairs))
+
+    _write_output(source_dir, output_lines)
 
 
 if __name__ == '__main__':
