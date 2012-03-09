fd27f17198e8385b21560611afede4628875bbf9
Michael Lossos <mlossos@pobox.com>
2012-03-08 21:43:26 -0500

    Pascal's triangle test.

--- src/python/pascalstriangle.py
+++ src/python/pascalstriangle.py
@@ -10,6 +10,7 @@ Generate Pascal's triangle.
 
 """
 import sys
+import itertools
 
 def _triangle_recursive(height, previous_row, result):
 	if height <= 0:
@@ -26,7 +27,7 @@ def _triangle_recursive(height, previous_row, result):
 		curr_row.append(1)
 
 	result.append(curr_row)
-	_triangle(height-1, curr_row, result)
+	_triangle_recursive(height-1, curr_row, result)
 
 def pascals_triangle_recursive(height):
 	result = []
@@ -34,11 +35,49 @@ def pascals_triangle_recursive(height):
 	return result
 
 
-if __name__ == '__main__':
-	height = 6
+
+
+
+
+############################################
+# Tests 
+#
+
+EXPECTED_VALUES = [
+	[1],
+	[1, 1],
+	[1, 2, 1],
+	[1, 3, 3, 1],
+	[1, 4, 6, 4, 1],
+	[1, 5, 10, 10, 5, 1],
+	[1, 6, 15, 20, 15, 6, 1],
+	[1, 7, 21, 35, 35, 21, 7, 1],
+	[1, 8, 28, 56, 70, 56, 28, 8, 1],
+	[1, 9, 36, 84, 126, 126, 84, 36, 9, 1],
+]
+
+def _hard_coded_validation(func):
+	actual_triangle = func(len(EXPECTED_VALUES))
+	for expected, actual in itertools.izip(EXPECTED_VALUES, actual_triangle):
+		assert expected == actual
+	assert len(EXPECTED_VALUES) == len(actual_triangle)
+
+def _run_tests():
+	_hard_coded_validation(pascals_triangle_recursive)
+	print 'SUCCESS All tests passed.'
+
+def main():
+	# Intentionally avoiding unittest (to run via main only) and argparse (for simpler code).
+	height = -1
 	if len(sys.argv) > 1:
 		height = int(sys.argv[1]) 
 
-	for row in pascals_triangle(height):
-		print row
+	if height < 0:
+		_run_tests()
+	else:
+		for row in pascals_triangle_recursive(height):
+			print row
+
+if __name__ == '__main__':
+	main()
 
