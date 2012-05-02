cfed958557d05b5537810f183aaa5efd10a78c8e
Michael Lossos <mlossos@pobox.com>
2012-05-02 00:30:08 -0700

    Dijkstra's flag sorter.

--- src/python/sortrgb.py
+++ src/python/sortrgb.py
@@ -32,23 +32,22 @@ This is a variant of the Dutch national flag problem (Dijkstra).
 
 """
 
-
-RGB_ORDER = {
-    'r': 0,
-    'g': 1,
-    'b': 2
-}
-
-def compare_gt(left, right):
-    return RGB_ORDER[left] > RGB_ORDER[right]
-
 def sort_rgb_bubble(rgb_values):
     """
     Bubble sort, one of the easiest to remember from university!
     Worst case amortized time complexity O(n**2). 
     The problem is for O(n) or better, so this is not the "right" answer. 
     """
-    #rgb_values = list(rgb_values)  # uncomment to avoid modifying the original list
+    RGB_ORDER = {
+        'r': 0,
+        'g': 1,
+        'b': 2
+    }
+
+    def compare_gt(left, right):
+        return RGB_ORDER[left] > RGB_ORDER[right]
+
+    rgb_values = list(rgb_values) # Remove this copy to fit the O(1) space constraint. Leaving this in allows us to use python strings instead of arrays, which is easier for testing.
     end = len(rgb_values)
     while end > 0:
         for x in xrange(0, end-1):
@@ -59,10 +58,32 @@ def sort_rgb_bubble(rgb_values):
 
         end -= 1
 
-    return rgb_values
+    return ''.join(rgb_values) # Convert from array back to string
 
-# Other sorting algorithms have O(n log n) amortized worst case time complexity, 
+# Other faster sorting algorithms have O(n log n) amortized worst case time complexity, 
 # which also do not meet the O(n) or better requirement.
+# Instead we turn to...
+
+def sort_rgb_dutch(rgb_values):
+    """
+    Dijkstra's flag sorter.
+    """
+    rgb_values = list(rgb_values) # See note above re. O(1) 
+    p = 0
+    q = len(rgb_values) - 1
+    i = 0
+    while i <= q:
+        if rgb_values[i] == 'r': # first value
+            rgb_values[i], rgb_values[p] = rgb_values[p], rgb_values[i]
+            p += 1
+            i += 1
+        elif rgb_values[i] == 'b': # middle value
+            rgb_values[i], rgb_values[q] = rgb_values[q], rgb_values[i]
+            q -= 1
+        else:
+            i += 1
+
+    return ''.join(rgb_values) # Convert from array back to string
 
 
 def test_sort(sort_func, expected, input):
@@ -73,10 +94,15 @@ def test_sort(sort_func, expected, input):
 
 def test_sort_funcs(expected, input):
     test_sort(sort_rgb_bubble, expected, input)
+    test_sort(sort_rgb_dutch, expected, input)
 
 if __name__ == '__main__':
-    test_sort_funcs(
-        ['r', 'r', 'r', 'g', 'g', 'g', 'b'], 
-        ['g', 'r', 'b', 'r', 'r', 'g', 'g' ])
+    test_sort_funcs('rrrgggb', 'grbrrgg')
+    test_sort_funcs('rrrrrgggbbb',  'bgrbgrbgrrr')
+
+
+
+
+
     
 
