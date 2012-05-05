3d61f18c873d9c8bb2028aa68a5e4fa50e333456
Michael Lossos <mlossos@pobox.com>
2012-05-05 13:00:27 -0700

    Added matchdocindex problem desc.

---
+++ src/python/matchdocindex.py
@@ -0,0 +1,22 @@
+"""
+Given a data structure that maps words to document indexes, return a list of document indexes that match all terms.
+
+e.g.
+
+Given:
+{ 
+    yummy: [0, 3, 7, 8]
+    japanese: [3, 5, 8]
+    ramen: [0, 1, 2, 3, 5, 8]
+}
+
+Return:
+[3, 8]
+
+This problem is a simplified presentation of word level inverted indexes. 
+For more background, you can also search for:
+- Word postings 
+- Term frequency inverse document frequency tf*idf. 
+- Book: Introduction to Information Retrieval by Manning et al
+"""
+
