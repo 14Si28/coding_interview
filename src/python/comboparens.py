39ad8f308f35ef1f090f10e2490ee5d11dc2fea2
Michael Lossos <mlossos@pobox.com>
2012-05-26 16:44:50 -0700

    Parenthesis permutations.

---
+++ src/python/comboparens.py
@@ -0,0 +1,32 @@
+"""
+Generate all permutations of balanced parenthesis given a quantity of pairs.
+
+* n = 1: ['()']
+* n = 2: ['()()', '(())']
+* n = 3: ['()(())', '((()))', '(()())', '(())()', '()()()']
+"""
+
+def gen_parens(n):
+    if n <= 1:
+        return [ '()' ]
+    
+    prev = gen_parens(n-1)
+    result = set()
+    for a in prev:
+        for index in xrange(len(a)):
+            curr = list(a)
+            curr.insert(index, '()')
+            result.add(''.join(curr))
+    
+    return result
+
+def gen_parens_strs(n):
+    lol = gen_parens(n)
+    results = []
+    for a in lol:
+        results.append(''.join(a))
+    return results
+
+if __name__ == '__main__':
+    for x in xrange(1, 5): 
+        print gen_parens_strs(x)
