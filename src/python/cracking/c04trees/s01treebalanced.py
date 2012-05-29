1738307acb7f05a8695bc40caa86f6aa4ad5fb8d
Michael Lossos <mlossos@pobox.com>
2012-05-28 19:10:14 -0700

    Alternate tree balance check.

--- src/python/cracking/c04trees/s01treebalanced.py
+++ src/python/cracking/c04trees/s01treebalanced.py
@@ -4,9 +4,9 @@ tree such that no two leaf nodes differ in distance from the root by more than o
 """
 
 
-def is_balanced(tree, threshold=1):
+def is_balanced2(tree, threshold=1):
 	"""
-	O(n)
+	O(n) time, O(log n) space.
 	"""
 	IMBALANCED = -1
 	def height(node):
@@ -28,7 +28,23 @@ def is_balanced(tree, threshold=1):
 	return height(tree) != IMBALANCED
 
 
-def _test_all():
+def is_balanced(tree, threshold=1):
+	"""
+	O(n^2) time.
+	"""
+	if not tree:
+		return True
+
+	def height(node):
+		if not node:
+			return 0
+
+		return 1 + max(height(node[1]), height(node[2]))
+
+	return abs(height(tree[1]) - height(tree[2])) <= threshold
+
+
+def _test_all(func):
 	tree2 = (8, 
                 (3, (1, None, None), 
                     (6, 
@@ -43,7 +59,7 @@ def _test_all():
                     )
                 )
             )
-	assert is_balanced(tree2)
+	assert func(tree2)
 	tree1 = (8, 
                 (3, None, None), 
                 (10, (9, None, None), 
@@ -53,7 +69,8 @@ def _test_all():
                     )
                 )
             )
-	assert not is_balanced(tree1)
+	assert not func(tree1)
 
 if __name__ == '__main__':
-	_test_all()
+	_test_all(is_balanced2)
+	_test_all(is_balanced)
