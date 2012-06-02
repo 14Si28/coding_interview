7e32fb40ceea02f77ac28901212870ca8c50ff4a
Michael Lossos <mlossos@pobox.com>
2012-06-01 23:53:26 -0700

    Availability.

--- src/python/search/gendata.py
+++ src/python/search/gendata.py
@@ -1,6 +1,7 @@
 import sys
 import csv
 import random
+import datetime
 
 def _property_description():
     return """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
@@ -27,13 +28,13 @@ def gen_properties(num_properties=10):
 def _stay_review():
     return """We had a nice stay. Blah. """ + 'Blah! '*random.randint(1,10)
 
-def gen_ratings(property_ids, min_ratings_each=0, max_ratings_each=9):
+def gen_ratings(property_ids, min_each=0, max_each=9):
     """
     returns: generator of lists of  [rating_id,property_id,user_id,rating,review]
     """
     next_rating_id = 1
     for property_id in property_ids:
-        num_ratings = random.randint(min_ratings_each, max_ratings_each)
+        num_ratings = random.randint(min_each, max_each)
         if num_ratings:
             for r in xrange(num_ratings):
                 values = [
@@ -45,6 +46,25 @@ def gen_ratings(property_ids, min_ratings_each=0, max_ratings_each=9):
                 ]
                 next_rating_id += 1
                 yield values
+
+def gen_availability(property_ids):
+    """
+    returns: generator of lists of [availability_id,property_id,start_date,end_date,price_per_night]
+    """
+    next_availability_id = 1
+    max_future_days = 30*6 # ~6 months
+    for property_id in property_ids:
+        start_date = datetime.date.today() + datetime.timedelta(random.randint(1,30))
+        end_date = start_date + datetime.timedelta(random.randint(1,max_future_days-1))
+        values = [
+            next_availability_id,
+            property_id,
+            start_date,
+            end_date,
+            random.randint(80,500) 
+        ]
+        next_availability_id += 1
+        yield values
 
 def print_properties():
     writer = csv.writer(sys.stdout)
@@ -59,10 +79,15 @@ def print_ratings(property_ids):
     for line in gen_ratings(property_ids):
         writer.writerow(line)
 
+def print_availability(property_ids):
+    writer = csv.writer(sys.stdout)
+    for line in gen_availability(property_ids):
+        writer.writerow(line)
+
 def main():
     property_ids = print_properties()
     print_ratings(property_ids)
-
+    print_availability(property_ids)
 
 if __name__ == '__main__':
     main()
