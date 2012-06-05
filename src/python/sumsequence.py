e7a1cc22ab521df42fd62abcce42893f57311c8c
Michael Lossos <mlossos@pobox.com>
2012-06-04 18:13:53 -0700

    Sum over a subsequence revised.

--- src/python/sumsequence.py
+++ src/python/sumsequence.py
@@ -7,30 +7,22 @@ Given: [-10, 1, 2, 5, -3]
 Answer: 8 
 from subsequence 1,2,5
 """
+import unittest
 
 def sumsequence(numbers):
-    min_sum = 0
-    max_sum = 0
-    for ind in xrange(len(numbers)):
-        min_sum += numbers[ind]
-        if max_sum < min_sum:
-            max_sum = min_sum
-        # elif min_sum < 0:
-        #     min_sum = 0
-    return max_sum
-
-def sumsequence2(numbers):
     """
     O(n)
     numbers: list of integers
     returns: largest sum of any subsequence in the list of numbers
     """
+    # TODO This does not work properly if all numbers are negative
     summer = list(numbers)
     min_sum = 0
     max_sum = 0
     for ind in xrange(len(summer)):
         if ind > 0: 
             summer[ind] += summer[ind - 1]
+
         if summer[ind] - min_sum > max_sum:
             max_sum = summer[ind] - min_sum
         if summer[ind] < min_sum:
@@ -38,11 +30,30 @@ def sumsequence2(numbers):
 
     return max_sum
 
+class TestSumSequence(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_positive_and_negative(self):
+        self.assertEquals(8, sumsequence([-10, 1, 2, 5, -3]))
+
+    def test_positive(self):
+        self.assertEquals(8, sumsequence([1, 2, 5]))
+
+    def test_zero(self):
+        self.assertEquals(0, sumsequence([0]))
+
+    def test_edge_single(self):
+        self.assertEquals(8, sumsequence([8]))
+
+    def test_edge_single_negative(self):
+        self.assertEquals(-8, sumsequence([-8]))
+
+    def test_negative(self):
+        self.assertEquals(-8, sumsequence([-1, -2, -5]))
+
 
 if __name__ == '__main__':
-    i = [-10, 1, 2, 5, -3]
-    print i
-    m = sumsequence(i)
-    print m
-    assert m == 8
+    unittest.main()
+
 
